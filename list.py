65baf4b5cfcfbb80967039363e789fd662c2f90c
rinkumittal3047-ship-it <rinkumittal3047@gmail.com>
2026-06-29 00:25:57 +0530

    new code

---
+++ list.py
@@ -0,0 +1,30 @@
+name=["rinku","khusbu","dimpal","deepika"]     #list is a collection which is ordered and changeable. Allows duplicate members.
+print(name)    #print the list
+print(type(name))    #by usimg type() functon we can find the data type of variable
+print(len(name))    #by using len() function we can find the length of list
+name.append("priti")    #by using append() function we can add the value in list
+print(name)    #print the list
+name.insert(2,"kajal")    #by using insert() function we can add the value in list at specific index
+print(name)    #print the list
+name.remove("kajal")    #by using remove() function we can remove the value from list
+print(name)    #print the list
+name.pop(3)    #by using pop() function we can remove the value from list at specific index
+print(name)    #print the list
+print(name[2])    #for accessing the list we can use indexing and slicing 
+print(name[3:])    #for accessing the list without explain the ending index
+print(name[-4:-2])    #for accessing the list without explain the starting index
+name[1]="priya"    #by using indexing we can change the value of list
+print(name)    #print the list
+name.sort()    #by using sort() function we can sort the list in ascending order
+print(name)    #print the list
+name.reverse()    #by using reverse() function we can sort the list in descending order
+print(name)    #print the list
+a=[1,2,3,4,5]    #list is a collection which is ordered and changeable. Allows duplicate members.
+b=[20,30,40,50,60]
+c=a+b    #by using + operator we can concatenate two lists
+print(c)    #print the list
+d=2*a
+print(d)    #print the list
+
+
+
