8c2fa281e80386b67dcab4a17bd501e9be237aad
rinkumittal3047-ship-it <rinkumittal3047@gmail.com>
2026-07-06 18:05:21 +0530

    updated practice.py

--- practice.py
+++ practice.py
@@ -3,11 +3,32 @@
 # # print(this_tuple[1:])
 # print(this_tuple[-4:-1])
 
-values=["rinku@123","dimpal@234","rinku@123","dimpal@234"]
-a=set(values)
-print(set)
-print(a)
+# values=["rinku@123","dimpal@234","rinku@123","dimpal@234"]
+# a=set(values)
+# print(set)
+# print(a)
 
+python={"rinku","khusbhu","dimpal","mahak",}
+SQL={"mahak","jiya","rinku","dimpal",}
+print(python.intersection(SQL))
+
+identity={
+    "person1": {
+        "name":"rinku",
+        "age":20,
+        "height":5.5,
+    },
+    "person2": {
+        "name":"priya",
+        "age":21,
+        "height":5.6,
+
+    }
+}
+print(identity["person1"]["name"])
+print(identity["person2"]["height"])
+
+print(identity["person2"].get("course","course not found"))
 
 
         
