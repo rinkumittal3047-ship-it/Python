b42b2692623a20fcdf25d289a8c1cab1976e74f3
rinkumittal3047-ship-it <rinkumittal3047@gmail.com>
2026-07-16 23:47:40 +0530

    add chnge

--- function question.py
+++ function question.py
@@ -1,185 +1,216 @@
-# def calculate(a,b=5):
-#     return a+b,a*b
-# x,y=calculate(4)
-# print(x,y)
-# print(calculate(4,2)[1])
+# 1.
+def calculate(a,b=5):
+    return a+b,a*b
+x,y=calculate(4)
+print(x,y)
+print(calculate(4,2)[1])
 
-# def display(a,b,c=10):
-#     print(a,b,c)
-# display(1,c=3,b=2)
-# display(4,5)
+# 2.
+def display(a,b,c=10):
+    print(a,b,c)
+display(1,c=3,b=2)
+display(4,5)
 
-# def calculate(number):
-#     if number%2==0:
-#         return number//2
-#     print("odd number")
-#     return number*3
-# print(calculate(8))
-# print(calculate(5))
+# 3.
+def calculate(number):
+    if number%2==0:
+        return number//2
+    print("odd number")
+    return number*3
+print(calculate(8))
+print(calculate(5))
 
-# def add_item(item,container=[]):
-#     container.append(item)
-#     return container
-# print(add_item(1))
-# print(add_item(2))
-# print(add_item(3,[]))
-# print(add_item(4))    
+# 4.
+def add_item(item,container=[]):
+    container.append(item)
+    return container
+print(add_item(1))
+print(add_item(2))
+print(add_item(3,[]))
+print(add_item(4))    
 
-# def analyse(first,*values):
-#     return first,max(values),sum(values[::2])  #:: means reverse the string
-# print(analyse(10,3,8,5,2))
+# 5.
+def analyse(first,*values):
+    return first,max(values),sum(values[::2])  #:: means reverse the string
+print(analyse(10,3,8,5,2))
 
-# def build_record(**data):
-#     data["total"]=sum(
-#         value
-#         for value in data.values()
-#         if isinstance(value,int)
-#         )
-#     return sorted(data.items())
-# print(build_record(a=2,b=3,name="x"))
+# 6.
+def build_record(**data):
+    data["total"]=sum(
+        value
+        for value in data.values()
+        if isinstance(value,int)
+        )
+    return sorted(data.items())
+print(build_record(a=2,b=3,name="x"))
 
-# def calculate(a,b,c):
-#     return a+b*c
-# values=(2,3)
-# options={"c":4}
-# print(calculate(*values,**options))
+# 7.
+def calculate(a,b,c):
+    return a+b*c
+values=(2,3)
+options={"c":4}
+print(calculate(*values,**options))
 
-# def report(name,*,score=0,passed=True):
-#     return f"{name}:{score}:{passed}"
-# print(report("riya",score=88))
-# print(report("kabir",passed=False,score=40))
+# 8.
+def report(name,*,score=0,passed=True):
+    return f"{name}:{score}:{passed}"
+print(report("riya",score=88))
+print(report("kabir",passed=False,score=40))
 
-# def change(number):
-#     number+=10
-#     return number
-# value=5
-# print(change(value),value)
+# 9.
+def change(number):
+    number+=10
+    return number
+value=5
+print(change(value),value)
 
-# def update(data):
-#    data[(0)]+=5
-#    data.append(sum(data))
-# numbers=[1,2,3]
-# update(numbers)
-# print(numbers)
+# 10.
+def update(data):
+   data[(0)]+=5
+   data.append(sum(data))
+numbers=[1,2,3]
+update(numbers)
+print(numbers)
 
-# def update(data):
-#     data=data+[4]
-#     data[0]=99
-#     return data
-# numbers=[1,2,3]
-# new_numbers=update(numbers)
-# print(numbers)
-# print(new_numbers)
+# 11.
+def update(data):
+    data=data+[4]
+    data[0]=99
+    return data
+numbers=[1,2,3]
+new_numbers=update(numbers)
+print(numbers)
+print(new_numbers)
 
-# def update(record):
-#     record["b"]=record.get("b",0)+2
-#     record={"c":3}
-#     return record
-# data={"a":1,"b":4}
-# result=update(data)
-# print(data)
-# print(result)
+# 12.
+def update(record):
+    record["b"]=record.get("b",0)+2
+    record={"c":3}
+    return record
+data={"a":1,"b":4}
+result=update(data)
+print(data)
+print(result)
 
-# def modify(data):
-#     data[1].append(30)
-#     return data+("done",)
-# values=(10,[20])
-# result=modify(values)
-# print(values)
-# print(result)
+# 13.
+def modify(data):
+    data[1].append(30)
+    return data+("done",)
+values=(10,[20])
+result=modify(values)
+print(values)
+print(result)
 
-# x=10
-# def outer():
-#     x=20
-#     def inner():
-#         global x
-#         x+=5
-#         print(x)
-#     inner()
-#     print(x)
-# outer()
-# print(x)
+# 14.
 
-# def outer():
-#     number=1
-#     def inner():
-#         nonlocal number
-#         number*=3
-#         return number
-#     print(inner(),inner())
-# outer()
+x=10
+def outer():
+    x=20
+    def inner():
+        global x
+        x+=5
+        print(x)
+    inner()
+    print(x)
+outer()
+print(x)
 
-# def create_power(exponent):
-#     def calculate(number):
-#         return number**exponent
-#     return calculate
-# square=create_power(2)
-# cube=create_power(3)
-# print(square(4)+cube(2))
+# 15.
+def outer():
+    number=1
+    def inner():
+        nonlocal number
+        number*=3
+        return number
+    print(inner(),inner())
+outer()
 
+# 16.
+def create_power(exponent):
+    def calculate(number):
+        return number**exponent
+    return calculate
+square=create_power(2)
+cube=create_power(3)
+print(square(4)+cube(2))
+
+# 17.
 # functions=[]
 # for number in range(3):
 #     functions.append(lambda:number)
 #     print([function() for function in functions])
 
-#     def apply(function,values):
-#         return[
-#             function(value)
-#             for value in values
-#             if function(value)>5
-#         ]
-#     def transform(number):
-#         return number*number-1
-#     print(apply(transform,[1,2,3,4]))
+# 18.
+functions=[]
+for number in range(3):
+    functions.append(lambda number=number:number)
+print(function() for function in functions)
 
-# numbers=[1,2,3,4,5]
-# result=list(
-#      map(
-#         lambda number:number*2,
-#         filter(lambda number:number%2==1,numbers)
-#     )
-# )
-# print(result)
+# 19.
+def apply(function,values):
+        return[
+            function(value)
+            for value in values
+            if function(value)>5
+        ]
+def transform(number):
+        return number*number-1
+print(apply(transform,[1,2,3,4]))
 
-# def adda(a,b):
-#     return a*b
-# def multiply(a,b):
-#     return a+b
-# operations={
-#     "addition": add,
-#     "multiplication":multiply
-# }
-# result=(
-#     operations["addition"](2,3)
-#     +operations["multiplication"](2,3)
-# )        
-# print(result)
+# 20.
+numbers=[1,2,3,4,5]
+result=list(
+     map(
+        lambda number:number*2,
+        filter(lambda number:number%2==1,numbers)
+    )
+)
+print(result)
 
-# def calculate(number):
-#     if number<=1:
-#         return 1
-#     return number + calculate(number-2)
-# print(calculate(6))
+# 21.
+def add(a,b):
+    return a*b
+def multiply(a,b):
+    return a+b
+operations={
+    "addition": add,
+    "multiplication":multiply
+}
+result=(
+    operations["addition"](2,3)
+    +operations["multiplication"](2,3)
+)        
+print(result)
 
-# def trace(number):
-#     if number==0:
-#         return 
-#     print(number,end=" ")
-#     trace(number-1)
-#     print(number,end=" ")
-# trace(3)
+# 22.
+def calculate(number):
+  if number<=1:
+        return 1
+  return number + calculate(number-2)
+print(calculate(6))
 
-# def flatten(data):
-#     result=[]
-#     for item in data:
-#         if isinstance(item,list):
-#             result.extend(flatten(item))
-#         else:
-#             result.append(item)
-#         return result
-#     values=[1,[2.[3,4]],5]
-#     print(flatten(values))
+# 23.
+def trace(number):
+   if number==0:
+    return 
+    print(number,end=" ")
+    trace(number-1)
+   print(number,end=" ")
+trace(3)
 
+# 24.
+def flatten(data):
+    result=[]
+    for item in data:
+      if isinstance(item,list):
+       result.extend(flatten(item))
+      else:
+         result.append(item)
+      return result
+    values=[1,[2.[3,4]],5]
+    print(flatten(values))
+
+# 25.
 def sequence(number):
     while number>0:
         yield number
@@ -188,6 +219,7 @@ result=sequence(5)
 print(next(result))
 print(list(result))
 
+# 26.
 def increase(function):
     def wrapper(number):
         return function(number)+1
@@ -197,3 +229,44 @@ def square(number):
     return number*number
 print(square(4))
 
+# 27.
+def double(function):
+    def wrapper(number):
+        return 2*function(number)
+    return wrapper
+def add_one(function):
+    def wrapper(number):
+        return function(number)+1
+    return wrapper
+@double
+@add_one
+def calculate(number):
+    return  number
+print(calculate(5))
+
+# 28.
+def combine(a:int,b:int)->int:
+    return str(a)+str(b)
+result=combine(2,3)
+print(result)
+print(type(result).__name__)
+
+# 29.
+x=10
+def display():
+    print(x)
+    x=20
+display()
+
+# 30.
+def modify(values):
+    for index,value in enumerate(values):
+        if value % 2==0:
+            values[index]=value//2
+        else:
+            values[index]=value*3+1
+            return tuple(reversed(values))
+    numbers=[1,2,3,4]
+    result=modify(numbers)
+    print(numbers)
+    print(result)
