98c47856e90e905be24ea6712442bff3c84cea5a
rinkumittal3047-ship-it <rinkumittal3047@gmail.com>
2026-07-17 14:07:37 +0530

    update.py

---
+++ file handling.py
@@ -0,0 +1,14 @@
+# Files: store the data 
+# to read,write the data used the file for large dataset
+# to sotre the permanent data and retrieve the data
+# variables only store the temporary data.
+
+# opening file :used open() function
+# file_object=open(file_name,mode,encoding)
+#  "file_name": the name or path of the file
+# mode: to read,  write 
+# encoding: utf-8 
+
+# open,close,with
+# read,character,readline,readlines(list format m answer show hoga),strip()(only in readline but it use in readlines to use loop),line count
+# character and words count(split,splitlines)
